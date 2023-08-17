8dab7f91870a6aaa6e4a4186863965554d468f7b
Filamer Doronio <filamerdoronio@gmail.com>
2023-08-16 21:18:16 -0700

    removing test

--- pages/index.js
+++ pages/index.js
@@ -121,7 +121,7 @@ const Page = () => {
 
             <Section delay={.4}>
                 <Heading as="h3" variant="section-title">
-                    On the Web Test
+                    On the Web
                 </Heading>
             </Section>
         </Container>

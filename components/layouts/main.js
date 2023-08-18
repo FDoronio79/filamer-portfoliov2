b97ad7d875e4f86b811396b173786d210aad5e60
Filamer Doronio <filamerdoronio@gmail.com>
2023-08-17 18:45:30 -0700

    added 3d voxel animation to site

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,14 @@
 import Head from 'next/head';
+import dynamic from 'next/dynamic';
 import Navbar from '../navbar'
+import NoSsr from '../no-ssr';
 import { Box, Container } from '@chakra-ui/react';
+import VoxelLaptopLoader from '../voxel-laptop-loader';
+
+const LazyVoxelLaptop = dynamic(() => import('../voxel-laptop'), {
+    ssr: false,
+    loading: () => <VoxelLaptopLoader />
+})
 
 const Main = ({ children, router }) => {
     return (
@@ -13,6 +21,9 @@ const Main = ({ children, router }) => {
             <Navbar path={router.asPath} />
 
             <Container maxW="container.md" pt={14}>
+                <NoSsr>
+                    <LazyVoxelLaptop />
+                </NoSsr>
                 {children}
             </Container>
         </Box>

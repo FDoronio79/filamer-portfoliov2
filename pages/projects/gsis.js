623842bc9bfefaddd8a6f5e3fd4c0052a5372f38
Filamer Doronio <filamerdoronio@gmail.com>
2023-08-22 10:21:56 -1000

    linting

--- pages/projects/gsis.js
+++ pages/projects/gsis.js
@@ -1,11 +1,9 @@
 import {
     Container,
     Badge,
-    Link,
     List,
     ListItem,
 } from '@chakra-ui/react'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, ProjectImage, Meta } from '../../components/projects'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'

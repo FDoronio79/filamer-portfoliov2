d52e7506e4000545a3d1ba3889b9854e21abd98d
Filamer Doronio <filamerdoronio@gmail.com>
2023-08-22 08:22:12 -1000

    fixed navbar link highlight

--- components/navbar.js
+++ components/navbar.js
@@ -17,17 +17,19 @@ import {
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons';
 import ThemeToggleButton from './theme-toggle-button';
+import { IoLogoGithub } from 'react-icons/io5'
 
 
 const LinkItem = ({ href, path, target, children, ...props }) => {
     const active = path === href
-    const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+    const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
     return (
         <Link
             as={NextLink}
             href={href}
+            scroll={false}
             p={2}
-            bg={active ? 'glassTeal' : undefined}
+            bg={active ? 'grassTeal' : undefined}
             color={active ? '#202023' : inactiveColor}
             target={target}
             {...props}
@@ -50,19 +52,19 @@ const Navbar = props => {
             as="nav"
             w="100%"
             bg={useColorModeValue('#ffffff40', '#20202380')}
-            style={{ backdropFilter: 'blur(10px)' }}
-            zIndex={1}
+            css={{ backdropFilter: 'blur(10px)' }}
+            zIndex={2}
             {...props}
         >
-            <Container 
-                display="flex" 
-                p={2} 
-                maxW="container.md" 
-                wrap="wrap" 
-                align="center" 
+            <Container
+                display="flex"
+                p={2}
+                maxW="container.md"
+                wrap="wrap"
+                align="center"
                 justify="space-between"
             >
-                <Flex 
+                <Flex
                     align="center"
                     mr={5}
                 >
@@ -75,41 +77,53 @@ const Navbar = props => {
                     </Heading>
                 </Flex>
 
-                <Stack 
+                <Stack
                     direction={{ base: 'column', md: 'row' }}
-                    display={{ base: 'none', md:'flex' }}
+                    display={{ base: 'none', md: 'flex' }}
                     width={{ base: 'full', md: 'auto' }}
                     alignItems="center"
                     flexGrow={1}
                     mt={{ base: 4, nmd: 0 }}
                 >
-                <LinkItem href="/projects" path={path}>
-                    Portfolio
-                </LinkItem>
-                <LinkItem href="/resume" path={path}>
-                    Resume
-                </LinkItem>
+                    <LinkItem href="/projects" path={path}>
+                        Portfolio
+                    </LinkItem>
+                    <LinkItem href="/resume" path={path}>
+                        Resume
+                    </LinkItem>
+                    <LinkItem
+                        target="_blank"
+                        href="https://github.com/FDoronio79"
+                        path={path}
+                        display="inline-flex"
+                        alignItems="center"
+                        style={{ gap: 4 }}
+                        pl={2}
+                    >
+                        <IoLogoGithub />
+                        Github
+                    </LinkItem>
                 </Stack>
                 <Box flex={1} align="right">
                     <ThemeToggleButton />
-                    <Box ml={2} display={{ base: 'inline-block', md: 'none'}}>
+                    <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
                         <Menu>
-                            <MenuButton 
-                            as={IconButton}
-                            icon={<HamburgerIcon />}
-                            variant="outline"
-                            aria-label="Options"
-                        />
+                            <MenuButton
+                                as={IconButton}
+                                icon={<HamburgerIcon />}
+                                variant="outline"
+                                aria-label="Options"
+                            />
                             <MenuList>
-                                    <MenuItem as={MenuLink} href="/">
+                                <MenuItem as={MenuLink} href="/">
                                     About
-                                    </MenuItem>
-                                    <MenuItem as={Link} href="/projects">
+                                </MenuItem>
+                                <MenuItem as={Link} href="/projects">
                                     Portfolio
-                                    </MenuItem>
-                                    <MenuItem as={Link} href="/resume">
+                                </MenuItem>
+                                <MenuItem as={Link} href="/resume">
                                     Resume
-                                    </MenuItem>
+                                </MenuItem>
                                 <MenuItem as={Link} href="https://github.com/FDoronio79" target="_blank">
                                     Github
                                 </MenuItem>
